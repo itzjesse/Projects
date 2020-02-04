ab65327124f35f8cb5b3cfb14571c6520befdd1e
O-T-O-Z <60617932+O-T-O-Z@users.noreply.github.com>
2020-02-04 17:45:19 +0100

    posities toegevoegd
    Werkt voor de grote getallen, nog geen opslaan van kleinere getallen

--- signals.c
+++ signals.c
@@ -10,39 +10,55 @@
 #include "LibStack.h"
 
 int main() {
-    int signals, i, j, sLength, max, value, start, end;
-    max = 0;
+    int signals, sLength, value, start, end;
+    int max = 0;
+    int pos = 0; //init position is zero
+    int pos1 = 0;
+    int previous = 0;
+    int h = 0;
     
     Stack stack;                        // initialize stack
     stack = newStack(10);
     
-    
-    scanf("%d",&signals);
+    scanf("%d", &signals);
 
-    for(i=0; i<signals; i++){
-        scanf("%d",&sLength);
-        for(j=0; j<sLength; j++){
-            scanf("%d",&value);
-            if(value<max){          // hier al die shit poppen
-            int end = pop(&stack);
-            
-
-                while(max>=value){
-                    if(pop(&stack)<end){
-                        //    printf("[%d,%d)@%d,   ", .. stack[start] stack[end]);
-                        
-
+    for (int i = 0; i < signals; i++) {
+        scanf("%d", &sLength);
+        for (int j = 0; j < sLength; j++) {
+            scanf("%d", &value);
+            if (value < max) {          // hier al die shit poppen
+                end = pop(&stack);
+                previous = pop(&stack);
+                pos--;
+                h = end;
+                while (previous <= end) {
+                    if (previous < end) {
+                        printf("[%d,%d)@%d\n", pos, pos1, h);
+                        break;
+                    } else if (previous == end) {
+                        while (previous == end) {
+                            pos--;
+                            previous = pop(&stack);
+                        }
+                        printf("[%d,%d)@%d\n", pos, pos1, h);
                     }
+                    h--;
                 }
-                
-                printf();
+                push(value, &stack);
+                pos++;
+            } else {  //stack the value
+                max = value;
+                push(value, &stack);
+                //printf("%d\n", pos);
+                pos++;  //When something gets added to the stack, the position moves one to the right (+1)
+                pos1++;
             }
             
-            max = value;
-            push(value, &stack);
-            
+            if (j == (sLength-1)) {
+                //
+            }
         }
-        
     }
+    
   return 0;
 }
