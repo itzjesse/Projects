18974619555e3195ea144716a7bf4d22a4e55a71
O-T-O-Z <60617932+O-T-O-Z@users.noreply.github.com>
2020-02-06 12:54:51 +0100

    Add files via upload

--- signals.c
+++ signals.c
@@ -9,10 +9,6 @@
  */
 #include "LibStack.h"
 
-\\ oefen code
-\\ 1 9 1 3 3 5 5 7 7 4 0
-\\ output: [5,7)@7, [5,7)@6, [3,7)@5, [3,9)@4, [1,9)@3, [1,9)@2, [0,9)@1, 
-
 int main() {
     int signals, sLength, value, start, end;
     int max = 0;
@@ -21,7 +17,7 @@ int main() {
     int previous = 0;
     int h = 0;
     int x = 1;
-    
+    int diff = 0;
     Stack stack;                        // initialize stack
     stack = newStack(10);
     
@@ -32,7 +28,15 @@ int main() {
         for (int j = 0; j < sLength; j++) {
             scanf("%d", &value);
             h = max;
-            while (value < h) {
+            while (value < h && (pos == 0 || pos == 1)) {
+                if (pos == 0) {
+                    printf("[%d,%d)@%d\n", pos, pos1, h);
+                    break;
+                } else if (pos == 1) {      //bijv 3 3 2 werkt nog niet
+                    //hier nog
+                }
+            }
+            while (value < h && pos > 1) {
                 if (x == 1) {
                     pop(&stack);
                     x++;
@@ -40,6 +44,7 @@ int main() {
                 previous = pop(&stack);
                 if (previous == h) {
                     pos--;
+                    diff++;
                     continue;
                 } else if (previous < h){
                     push(previous, &stack);
@@ -50,12 +55,19 @@ int main() {
                     break;
                 }
             }
+            
             push(value, &stack);
+            pos += diff;
+            diff = 0;
             max = value;       //de top van de stack is het hoogste getal en als er eentje wordt toegevoegd is deze groter of gelijk dus altijd de max value
             pos++;             //When something gets added to the stack, the position moves one to the right (+1)
             pos1++;
             x = 1;
+            if (j == sLength - 1) {
+                //en hier nog
+            }
         }
     }
+    freeStack(stack);
     return 0;
 }
